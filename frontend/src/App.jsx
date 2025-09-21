f05f0ac0cdb6b13f42f2b5d2f66ede6fabf35461
Kyohei <tatsukawa929@gmail.com>
2025-09-21 13:15:35 +0900

    add backend frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex-col p-3 justify-center justify-items-center text-black text-4xl bg-gray-300'>
+      <div className='m-3'>
+             One
+      </div>
+      <div className='m-3'>
+             Two
+      </div>
+      <div className='m-3'>
+             Three
+      </div>
+    </div>
+  )
+}
+
+export default App
